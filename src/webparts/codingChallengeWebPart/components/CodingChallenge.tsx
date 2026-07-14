1317229fa766ac3f3776a1f37a136145bd7d09ca
tFiedlerWenite <t.fiedler@wenite.de>
2026-07-14 17:44:48 +0200

    redux example

--- src/webparts/codingChallengeWebPart/components/CodingChallenge.tsx
+++ src/webparts/codingChallengeWebPart/components/CodingChallenge.tsx
@@ -1,44 +1,27 @@
-import * as React from 'react';
-import styles from './CodingChallenge.module.scss';
-import type { ICodingChallengeProps } from './ICodingChallengeProps';
-import { escape } from '@microsoft/sp-lodash-subset';
-import * as strings from 'CodingChallengeWebPartStrings';
+import * as React from "react";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import { CounterState, increment } from "../store/slice";
+import { PrimaryButton, Stack, Text } from "@fluentui/react";
+import { store } from "../store/store";
 
-export default class CodingChallenge extends React.Component<ICodingChallengeProps> {
-  public render(): React.ReactElement<ICodingChallengeProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
+export default function CodingChallenge(): React.ReactElement {
+  return (
+    <Provider store={store}>
+      <CodingChallengeComponent />
+    </Provider>
+  );
+}
 
-    return (
-      <section className={`${styles.codingChallenge} ${hasTeamsContext ? styles.teams : ''}`}>
-        <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-          <h2>{strings.GreetingMessage.replace(/\{0\}/g, escape(userDisplayName))}</h2>
-          <div>{environmentMessage}</div>
-          <div>{strings.PropertyValueLabel}{' '}<strong>{escape(description)}</strong></div>
-        </div>
-        <div>
-          <h3>{strings.WelcomeTitle}</h3>
-          <p>
-            {strings.WelcomeDescription}
-          </p>
-          <h4>{strings.LearnMoreHeading}</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">{strings.LinkOverview}</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">{strings.LinkMicrosoftGraph}</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">{strings.LinkMicrosoftTeams}</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">{strings.LinkVivaConnections}</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">{strings.LinkMarketplace}</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">{strings.LinkApiReference}</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">{strings.LinkDeveloperCommunity}</a></li>
-          </ul>
-        </div>
-      </section>
-    );
-  }
+function CodingChallengeComponent(): React.ReactElement {
+  const counter = useSelector((state: CounterState) => state.value);
+  const dispatch = useDispatch();
+
+  return (
+    <Stack>
+      <Text>Zähler: {counter}</Text>
+      <PrimaryButton onClick={() => dispatch(increment())}>
+        Inkrementieren
+      </PrimaryButton>
+    </Stack>
+  );
 }
